f2e6a088d178d5621538e4cd837ee341e8d9bf99
Seraph <42664208+Seraph27@users.noreply.github.com>
2024-02-03 20:49:29 -0800

    preittier error elint
    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,12 @@ module.exports = {
     tsconfigRootDir: __dirname,
   },
   rules: {
-    'prettier/prettier': 'error',
+    "prettier/prettier": [
+      'error',
+      {
+        "endOfLine": "auto"
+      }
+    ],
     'react/react-in-jsx-scope': 'off',
     'jsx-a11y/anchor-is-valid': [
       'error',
