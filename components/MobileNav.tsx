6fe8cd1ddc1d4d43020ae1313745eb7f72ba3bb0
Seraph <42664208+Seraph27@users.noreply.github.com>
2024-03-25 02:59:02 -0700

    feat: reworked mobile nav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,76 +1,72 @@
 'use client'
 
-import { useState } from 'react'
 import Link from './Link'
-import headerNavLinks from '@/data/headerNavLinks'
+import { CupSoda, Menu } from 'lucide-react'
+import { Button } from './shadcn/button'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/shadcn/sheet"
 
 const MobileNav = () => {
-  const [navShow, setNavShow] = useState(false)
-
-  const onToggleNav = () => {
-    setNavShow((status) => {
-      if (status) {
-        document.body.style.overflow = 'auto'
-      } else {
-        // Prevent scrolling
-        document.body.style.overflow = 'hidden'
-      }
-      return !status
-    })
-  }
-
   return (
     <>
-      <button aria-label="Toggle Menu" onClick={onToggleNav} className="sm:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="h-8 w-8 text-gray-900 dark:text-gray-100"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
-      {/* needs fixing (idk why translate-x-full shows on desktop) */}
-      <div
-        className={`fixed left-0 top-0 z-10 h-full w-full transform bg-white opacity-95 duration-300 ease-in-out dark:bg-gray-950 dark:opacity-[0.98] ${
-          navShow ? 'translate-x-0' : 'hidden'
-        }`}
-      >
-        <div className="flex justify-end">
-          <button className="mr-8 mt-11 h-8 w-8" aria-label="Toggle Menu" onClick={onToggleNav}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="text-gray-900 dark:text-gray-100"
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+              variant="outline"
+              size="icon"
+              className="shrink-0 md:hidden"
             >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-        </div>
-        <nav className="fixed mt-8 h-full">
-          {headerNavLinks.map((link) => (
-            <div key={link.title} className="px-12 py-4">
+              <Menu className="h-5 w-5" />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+        </SheetTrigger>
+        <SheetContent side="top">
+          <nav className="grid gap-6 text-lg font-medium">
+            <SheetClose asChild>
               <Link
-                href={link.href}
-                className="text-2xl font-bold tracking-widest text-gray-900 dark:text-gray-100"
-                onClick={onToggleNav}
+                href="/"
+                className="flex items-center gap-2 text-lg font-semibold"
               >
-                {link.title}
+                <CupSoda className="h-6 w-6" />
+                <span className="sr-only">Home</span>
               </Link>
-            </div>
-          ))}
-        </nav>
-      </div>
+            </SheetClose>
+            <SheetClose asChild>
+              <Link href="/blog" className="hover:text-indigo-200">
+                Blog
+              </Link>
+            </SheetClose>
+            <SheetClose asChild>
+              <Link
+                href="/codeforces"
+                className="text-zinc-200 hover:text-indigo-200"
+              >
+                Codeforces
+              </Link>
+            </SheetClose>
+            <SheetClose asChild>
+              <Link
+                href="/projects"
+                className="text-zinc-200 hover:text-indigo-200"
+              >
+                Projects
+              </Link>
+            </SheetClose>
+            <SheetClose asChild>
+              <Link
+                href="/about"
+                className="text-zinc-200 hover:text-indigo-200"
+              >
+                About
+              </Link>
+            </SheetClose>     
+          </nav>
+        </SheetContent>
+      </Sheet>     
     </>
   )
 }
