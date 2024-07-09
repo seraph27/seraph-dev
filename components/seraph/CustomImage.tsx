67ea82474597f120de6666551ab39fa377c442c1
Seraph <rexchao27@Gmail.com>
2024-07-09 22:59:16 +0800

    feat: summer internship week 1 blog

--- components/seraph/CustomImage.tsx
+++ components/seraph/CustomImage.tsx
@@ -1,11 +1,11 @@
 'use client'
 import Image from 'next/image'
 
-const CustomImage = ({ imgSrc }) => {
+const CustomImage = ({ imgSrc, width=600, height=600}) => {
   return (
-    <div className="flex justify-center rounded-md leading-normal">
-      <div className="rounded-lg">
-        {imgSrc ? <Image alt={'asd'} src={imgSrc} width={600} height={600} quality={100} className="rounded-lg"/> : null}
+    <div className="flex justify-center ">
+      <div className="">
+        {imgSrc ? <Image alt={'asd'} src={imgSrc} width={width} height={height} quality={100} className="rounded-lg"/> : null}
       </div>
     </div>
   )
