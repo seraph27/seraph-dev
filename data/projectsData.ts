440e8c3a293541e53c3330eee1eb11ff39a29ccc
Seraph27 <rexchao27@Gmail.com>
2024-09-30 15:13:41 -0700

    fix: minor changes

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -11,12 +11,12 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'Codeforces: Blue coder by end of 2024',
-    description: `Still far away but on the grind`,
-    imgSrc: '/static/images/cp/profile.png',
+    title: 'REFACTORING',
+    description: ``,
+    imgSrc: '/static/images/thumbnails/bg.jpg',
     href: './codeforces',
     hoverTitle: 'Why competitive programming?',
-    hoverDesc: 'In short, competitive programming is a mind sport where people design algorthims to a set of inputs in order to output the correct answer, and it was very inspiring to see people at the highest level being able to visualize complex algorthims effortlessly. \n\nTldr: I want to get good too',
+    hoverDesc: 'placeholder',
     hoverImgSrc: '/static/images/cp/fubuki.png',
     startDate: 'January 2024',
   },
